37b124d9984e93759eefd661f2a9ac0f1c5d02d6
Mboh Bless Pearl <63162575+MbohBless@users.noreply.github.com>
2024-08-24 00:22:40 +0200

    Update serverlessEndpoint.ts

--- server/src/embeddings/serverlessEndpoint.ts
+++ server/src/embeddings/serverlessEndpoint.ts
@@ -2,11 +2,11 @@ import fetch from 'node-fetch';
 
 const getTermEmbeddings = async (query) => {
     const apiKey = process.env.EMBEDDING_KEY;
-    const url = `https://us-central1-projectphoenix-verteximage.cloudfunctions.net/getEmbeddings/embeddings?apikey=${apiKey}&arg=${query}`;
+    const url = process.env.EMBEDDING_URL;
 
     const response = await fetch(url).then((res) => res.json());
 
-    return response;
+    return response["embedding"];
 };
 
 export default getTermEmbeddings;
