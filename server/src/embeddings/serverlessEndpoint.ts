e8ddd673a7a7e7dacbb717d910804393b5012266
Mboh Bless Pearl <63162575+MbohBless@users.noreply.github.com>
2024-08-24 09:12:09 +0200

    Update serverlessEndpoint.ts

--- server/src/embeddings/serverlessEndpoint.ts
+++ server/src/embeddings/serverlessEndpoint.ts
@@ -2,7 +2,7 @@ import fetch from 'node-fetch';
 
 const getTermEmbeddings = async (query) => {
     const apiKey = process.env.EMBEDDING_KEY;
-    const url = process.env.EMBEDDING_URL;
+    const url = process.env.EMBEDDING_URL+"/api/embeddings";
 
     const response = await fetch(url).then((res) => res.json());
 
