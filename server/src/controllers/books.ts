32234a999332e354bf4d38d9201246c00eda0881
Polar <98938042+paulinechaga@users.noreply.github.com>
2024-08-24 11:14:49 +0000

    vector searches

--- server/src/controllers/books.ts
+++ server/src/controllers/books.ts
@@ -83,14 +83,30 @@ class BookController {
     }
 
     public async searchBooks(query: string): Promise<Book[]> {
-        const books = await collections?.books?.find(
+        // const books = await collections?.books?.find(
+        //     {
+        //         $or: [
+        //             {title: {$regex: new RegExp(query, 'i')}},
+        //             {'authors.name': {$regex: new RegExp(query, 'i')}},
+        //         ]
+        //     }).limit(25).toArray();
+        // return books;
+        const vector = await getEmbeddings(query);
+        console.log(vector)
+        const aggregationPipeline = [
             {
-                $or: [
-                    {title: {$regex: new RegExp(query, 'i')}},
-                    {'authors.name': {$regex: new RegExp(query, 'i')}},
-                ]
-            }).limit(25).toArray();
+                $vectorSearch: {
+                    queryVector: vector,
+                    path:'embeddings',
+                    numCandidates:100,
+                    index:'vector_index',
+                    limit:5,
+                }
+            }
+        ];
+        const books = await collections?.books?.aggregate(aggregationPipeline).toArray() as Book[];
         return books;
+
     }
 
     public async createBook(book: Book): Promise<InsertOneResult> {
